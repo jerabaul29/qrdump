4256b59dab877fed580d574538394326e27937b3
jerabaul29 <jean.rblt@gmail.com>
2020-03-05 13:36:54 +0100

    make sure testing cleans after itself

--- src/test_script.sh
+++ src/test_script.sh
@@ -5,10 +5,38 @@
 # TODO: automate testing
 # TODO: increase coverage
 
+##############################################
+# sounder programming environment            #
+##############################################
+
+# exit if a command fails
+set -o errexit
+# make sure to show the error code of the first failing command
+set -o pipefail
+# do not overwrite files too easily
+set -o noclobber
+# exit if try to use undefined variable
+set -o nounset
+
+########################################
+# helper functions                     #
+########################################
+
+press_any(){
+    echo "giving a chance to inspect the output"
+    read -n 1 -s -r -p "Press any key to continue"
+    echo " "
+}
+
 ########################################
 # test of encoding / decoding base64   #
 ########################################
 
 mkdir dummy
 cd dummy
+
 head -c 4096 </dev/urandom > dummy_file.dat
+
+press_any
+cd ..
+rm -rf dummy
