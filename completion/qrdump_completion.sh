cd31c079e681722b05c78d3627cc070bbb3f8ecd
jerabaul29 <jean.rblt@gmail.com>
2020-04-02 15:32:31 +0200

    Work on autocomplete: ok with options and relative paths

--- completion/qrdump_completion.sh
+++ completion/qrdump_completion.sh
@@ -9,16 +9,34 @@
 _qrdump(){
     COMPREPLY=()
 
-    local LIST_OPTIONS_NOARG="--help --verbose --base64 --debug --encode --decode --read-A4 --create-A4 --safe-mode --layout --extract"
-    local LIST_OPTIONS_ARG="--input --output --digest"
+    local LIST_OPTIONS_NOARG=" --help --verbose --base64 --debug --encode --decode --read-A4 --create-A4 --safe-mode --layout --extract "
+    local LIST_OPTIONS_ARG=" --input --output --digest "
 
     local CRRT_ENTRY="${COMP_WORDS[COMP_CWORD]}"
-    # TODO: case on this
-# if last word was an option that requires following path, provide paths
-# if last word was an option that does not require path, provide all options
-# if last word was path, provide all options
 
-    COMPREPLY=($(compgen -W "$LIST_OPTIONS_ARG $LIST_OPTIONS_NOARG" -- "$CRRT_ENTRY"))
+    local PREVIOUS_ENTRY_TYPE="None"
+    if [ "${COMP_CWORD}" -gt 1 ]; then
+        local PREVIOUS_INDEX=$(( ${COMP_CWORD} - 1 ))
+        local PREVIOUS_ENTRY="${COMP_WORDS[${PREVIOUS_INDEX}]}"
+
+        if [[ "${LIST_OPTIONS_ARG}" = *" ${PREVIOUS_ENTRY} "* ]]; then
+            PREVIOUS_ENTRY_TYPE="PATH_NEEDED"
+        fi
+    fi
+
+    # TODO: case on this
+    # if last word was an option that requires following path, provide paths
+    # if last word was an option that does not require path, provide all options
+    # if last word was path, provide all options
+    case "$PREVIOUS_ENTRY_TYPE" in
+        PATH_NEEDED)
+            # TODO: make work on absolute paths
+            COMPREPLY=($(compgen -f ${CRRT_ENTRY}))
+            ;;
+        *)
+            COMPREPLY=($(compgen -W "$LIST_OPTIONS_ARG $LIST_OPTIONS_NOARG" -- "$CRRT_ENTRY"))
+            ;;
+    esac
 }
 
 complete -o filenames -F _qrdump qrdump
