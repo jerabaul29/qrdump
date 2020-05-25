2da9b22ae2474b67f14d0a158732fcfd26cc65be
jerabaul29 <jean.rblt@gmail.com>
2020-05-25 23:01:47 +0200

    Run a full --parchive test

--- tests/test_script_to_from_A4_parchive.sh
+++ tests/test_script_to_from_A4_parchive.sh
@@ -9,7 +9,7 @@ DIGEST_IN=$(sha512sum dummy/dummy_file.dat | awk '{print $1;}')
 
 ./qrdump.sh --create-A4 --base64 --safe-mode --output ./dummy/generated_pdf/my_pdf.pdf --input ./dummy/dummy_file.dat --metadata "some blabla" --parchive
 
-./qrdump.sh --read-A4 --base64 --output ./dummy/restored_result/ --input ./dummy/generated_pdf/my_pdf.pdf
+./qrdump.sh --read-A4 --base64 --output ./dummy/restored_result/ --input ./dummy/generated_pdf/my_pdf.pdf --parchive
 
 DIGEST_OUT=$(sha512sum dummy/restored_result/dummy_file.dat | awk '{print $1;}')
 
