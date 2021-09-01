a10638a716024c118654f364c41b41b415e4dfe6
JRT <jean.rblt@gmail.com>
2021-09-01 12:42:36 +0200

    Add verbosity and debug to test

--- tests/test_script_to_from_A4.sh
+++ tests/test_script_to_from_A4.sh
@@ -7,9 +7,9 @@ mkdir ./dummy/restored_result
 
 DIGEST_IN=$(sha512sum dummy/dummy_file.dat | awk '{print $1;}')
 
-./qrdump.sh --create-A4 --base64 --output ./dummy/generated_pdf/my_pdf.pdf --input ./dummy/dummy_file.dat
+./qrdump.sh --debug --verbose --create-A4 --base64 --output ./dummy/generated_pdf/my_pdf.pdf --input ./dummy/dummy_file.dat
 
-./qrdump.sh --read-A4 --base64 --output ./dummy/restored_result/ --input ./dummy/generated_pdf/my_pdf.pdf
+./qrdump.sh --debug --verbose --read-A4 --base64 --output ./dummy/restored_result/ --input ./dummy/generated_pdf/my_pdf.pdf
 
 DIGEST_OUT=$(sha512sum dummy/restored_result/dummy_file.dat | awk '{print $1;}')
 
