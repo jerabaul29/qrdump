8e03ed95ed32630c3ce2b54e9b5e303d83f46bdb
JRT <jean.rblt@gmail.com>
2021-09-04 20:40:15 +0200

    Add exit message on problem with binary encoding

---
+++ tests/ub2104_test_consistency_extract_old_file.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+
+source ./setup_moveto_src.sh
+
+mkdir ./dummy/generated_pdf
+mkdir ./dummy/restored_result
+
+DIGEST_IN="3c1e723a2258d013e9a32b06da1b343547cd7dffd4993077516d643661f1aea4be3214b06da1ca08467a9f6d44b3cf41c9ecbf772642f82cdf3efe3f44218317"
+
+./qrdump.sh --read-A4 --output ./dummy/restored_result/ --input ../tests/dummy_file_binary.pdf
+
+DIGEST_OUT=$(sha512sum dummy/restored_result/dummy_file.dat | awk '{print $1;}')
+
+source ../tests/rigup_moveto_test.sh
